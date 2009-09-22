291b00af3f85f5b00efdbb5c1d4fdd5ae71b7ea2
sanyungui@yahoo.com.cn <sanyungui@yahoo.com.cn@6ccff402-a32b-11de-9ca8-173d21a9ba08>
2009-09-22 10:09:32 +0000

    版本自动更新开发中，实现解压文件！~~~

---
+++ trunk/src/TCG.Files/TCG.Files.Handlers/fileZip.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Web;
+using System.Threading;
+
+
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Checksums;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+
+namespace TCG.Files.Handlers
+{
+    public class fileZip
+    {
+        public void ZipFile(string FileToZip, string ZipedFile, int CompressionLevel, int BlockSize, string password)
+        {
+            //如果文件没有找到，则报错
+            if (!System.IO.File.Exists(FileToZip))
+            {
+                throw new System.IO.FileNotFoundException("The specified file " + FileToZip + " could not be found. Zipping aborderd");
+            }
+
+            System.IO.FileStream StreamToZip = new System.IO.FileStream(FileToZip, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            System.IO.FileStream ZipFile = System.IO.File.Create(ZipedFile);
+            ZipOutputStream ZipStream = new ZipOutputStream(ZipFile);
+            ZipEntry ZipEntry = new ZipEntry("ZippedFile");
+            ZipStream.PutNextEntry(ZipEntry);
+            ZipStream.SetLevel(CompressionLevel);
+            byte[] buffer = new byte[BlockSize];
+            System.Int32 size = StreamToZip.Read(buffer, 0, buffer.Length);
+            ZipStream.Write(buffer, 0, size);
+            try
+            {
+                while (size < StreamToZip.Length)
+                {
+                    int sizeRead = StreamToZip.Read(buffer, 0, buffer.Length);
+                    ZipStream.Write(buffer, 0, sizeRead);
+                    size += sizeRead;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            ZipStream.Finish();
+            ZipStream.Close();
+            StreamToZip.Close();
+        }
+
+        public void ZipFileMain(string[] args)
+        {
+            //string[] filenames = Directory.GetFiles(args[0]);
+            string[] filenames = new string[] { args[0] };
+
+            Crc32 crc = new Crc32();
+            ZipOutputStream s = new ZipOutputStream(File.Create(args[1]));
+
+            s.SetLevel(6); // 0 - store only to 9 - means best compression
+
+            foreach (string file in filenames)
+            {
+                //打开压缩文件
+                FileStream fs = File.OpenRead(file);
+                byte[] buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+                ZipEntry entry = new ZipEntry(file);
+
+                entry.DateTime = DateTime.Now;
+
+                // set Size and the crc, because the information
+                // about the size and crc should be stored in the header
+                // if it is not set it is automatically written in the footer.
+                // (in this case size == crc == -1 in the header)
+                // Some ZIP programs have problems with zip files that don't store
+                // the size and crc in the header.
+                entry.Size = fs.Length;
+                fs.Close();
+
+                crc.Reset();
+                crc.Update(buffer);
+
+                entry.Crc = crc.Value;
+
+                s.PutNextEntry(entry);
+
+                s.Write(buffer, 0, buffer.Length);
+
+            }
+            s.Finish();
+            s.Close();
+        }
+
+
+        /// 
+        /// 解压文件
+        /// 
+        /// 包含要解压的文件名和要解压到的目录名数组
+        public void UnZip(string[] args)
+        {
+            ZipInputStream s = new ZipInputStream(File.OpenRead(args[0]));
+            try
+            {
+                ZipEntry theEntry;
+                while ((theEntry = s.GetNextEntry()) != null)
+                {
+                    string directoryName = Path.GetDirectoryName(args[1]);
+                    string fileName = Path.GetFileName(theEntry.Name);
+
+                    //生成解压目录
+                    Directory.CreateDirectory(directoryName);
+
+                    if (fileName != String.Empty)
+                    {
+                        //解压文件到指定的目录
+                        FileStream streamWriter = File.Create(args[1] + fileName);
+                        int size = 2048;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(data, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        streamWriter.Close();
+                    }
+                }
+                s.Close();
+            }
+            catch (Exception eu)
+            {
+                throw eu;
+            }
+            finally
+            {
+                s.Close();
+            }
+        }//end UnZip
+
+        public static bool UnZipFile(string file, string dir)
+        {
+            try
+            {
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+                string fileFullName = Path.Combine(dir, file);
+                ZipInputStream s = new ZipInputStream(File.OpenRead(fileFullName));
+
+                ZipEntry theEntry;
+                while ((theEntry = s.GetNextEntry()) != null)
+                {
+                    string directoryName = Path.GetDirectoryName(theEntry.Name);
+                    string fileName = Path.GetFileName(theEntry.Name);
+
+                    if (directoryName != String.Empty)
+                        Directory.CreateDirectory(Path.Combine(dir, directoryName));
+                    if (fileName != String.Empty)
+                    {
+                        FileStream streamWriter = File.Create(Path.Combine(dir, theEntry.Name));
+                        int size = 2048;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(data, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        streamWriter.Close();
+                    }
+                }
+                s.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+    }
+
+}
---
+++ trunk/src/WebUI/Test.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using TCG.Pages;
+using TCG.Files.Handlers;
+
+using TCG.Utils;
+using TCG.Entity;
+using TCG.Release;
+
+public partial class Test :  Origin
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string fullName = @"E:\TCG.V9\src\WebUI\index.zip";//C:\test\a.zip 
+        //解压文件 
+        //AttachmentUnZip.UpZip(fullName); 
+        // string[] FileProperties = new string[2]; 
+        // FileProperties[0] = fullName;//待解压的文件 
+        // FileProperties[1] = System.IO.Path.GetDirectoryName(fullName);//解压后放置的目标目录 
+        // UnZipClass UnZc=new UnZipClass(); 
+        // UnZc.UnZip(FileProperties); 
+        //string dir = System.IO.Path.GetDirectoryName(fullName);
+        //string fileName = System.IO.Path.GetFileName(fullName);
+        //fileZip.UnZipFile(fileName, dir);
+
+        //List<VersionItem> vers = Versions.WebVersionHistory;
+
+    }
+
+    private void Zip()
+    {
+        string [] FileProperties = new string[2];
+        string fullName = @"E:\TCG.V9\src\WebUI\index.htm";
+        string destPath=System.IO.Path.GetDirectoryName(fullName);//C:\test 
+        //待压缩文件 
+        FileProperties[0]=fullName; 
+
+        //压缩后的目标文件 
+        FileProperties[1]= destPath +"\\"+ System.IO.Path.GetFileNameWithoutExtension(fullName) + ".zip";
+        fileZip Zc = new fileZip(); 
+        Zc.ZipFileMain(FileProperties); 
+    }
+}
