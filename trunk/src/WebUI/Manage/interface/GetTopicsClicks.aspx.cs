ff13cc86e2dbf9d1ca933f86548a59a2b3b8d89b
sanyungui@yahoo.com.cn <sanyungui@yahoo.com.cn@6ccff402-a32b-11de-9ca8-173d21a9ba08>
2009-10-07 05:38:27 +0000

    加入FLASH 相册

---
+++ trunk/src/WebUI/Manage/interface/GetTopicsClicks.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+using TCG.Utils;
+using TCG.Data;
+using TCG.Template.Handlers;
+using TCG.Pages;
+
+using TCG.News.Entity;
+using TCG.News.Handlers;
+
+public partial class Manage_interface_GetTopicsClicks : ScriptsMain
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            string topics = Fetch.Get("topics");
+            if (!string.IsNullOrEmpty(topics))
+            {
+                string SQL = "SELECT * FROM T_News_NewsInfo (NOLOCK) WHERE iId In (" + topics + ")";
+                DataTable dt = base.conn.GetDataTable(SQL);
+                if (dt == null) return;
+
+                string text = "";
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    string txx = i == dt.Rows.Count - 1 ? "" : ",";
+                    text += "{Id:\"" + dt.Rows[i]["iId"].ToString() + "\",Click:\"" + dt.Rows[i]["iCount"].ToString() + "\"}" + txx;
+                }
+                Response.Write(text);
+                base.conn.Close();
+            }
+        }
+    }
+}
---
+++ trunk/src/WebUI/js/news.js
@@ -0,0 +1,16 @@
+
+document.write("<li><a href=\"/news/2009/1003/DZ.NETluntanshangchangpiliangjiarudiqudijueben.htm\" target=\"_blank\">DZ.NET 论坛商场批量加入地区的脚本</a><span>09.08.03</span></li>");
+
+document.write("<li><a href=\"/news/2009/1003/SQLSERVERzuolianjiefuwuqidiyufa.htm\" target=\"_blank\">SQL SERVER做链接服务器的愈发</a><span>09.08.03</span></li>");
+
+document.write("<li><a href=\"/gonggao/2009/1002/caiyongshimefangshizanzhubenzhan？.htm\" target=\"_blank\"><font color='#008000'>采用什么方式赞助本站？</font></a><span>09.08.03</span></li>");
+
+document.write("<li><a href=\"/gonggao/2009/1002/benzhanchengxuyiquanbukaiyuan,xuexihuodangercikaifadipengyoukeyimianfeihuodeidaima.htm\" target=\"_blank\"><font color='#FF0000'>本站程序已全部开源,学习和当二次开发的朋友可以免费获得代码</font></a><span>09.08.03</span></li>");
+
+document.write("<li><a href=\"/news/2009/0930/jianrongIEhuoFFdiCSSwenzihuanxingxiangxixieyue.htm\" target=\"_blank\">兼容IE和FF的CSS文字换行详细解说</a><span>09.08.03</span></li>");
+
+document.write("<li><a href=\"/flashsandy/2009/0930/peizhiFlashDevelopdiAS3kaifahuanjing.htm\" target=\"_blank\">配置FlashDevelop的AS3开发环境</a><span>09.08.03</span></li>");
+
+document.write("<li><a href=\"/version/2009/0930/TCGCMSSystemxitongzuixinbanbenxiazai.htm\" target=\"_blank\">TCGCMSSystem系统最新版本下载</a><span>09.08.03</span></li>");
+
+document.write("<li><a href=\"/anli/2009/0930/xiangtandaixuezhangshaxiaoyoukuai.htm\" target=\"_blank\">湘潭大学长沙校友会</a><span>09.08.03</span></li>");
