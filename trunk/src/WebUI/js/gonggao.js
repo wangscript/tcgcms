92bb069e97aea71850a0676e91e4a61af39fc2b1
sanyungui@yahoo.com.cn <sanyungui@yahoo.com.cn@6ccff402-a32b-11de-9ca8-173d21a9ba08>
2009-10-08 07:30:55 +0000

    准备发布的第一版

--- trunk/src/WebUI/js/gonggao.js
+++ trunk/src/WebUI/js/gonggao.js
@@ -1,4 +1,4 @@
 
-document.write("<li><a href=\"/gonggao/2009/1002/caiyongshimefangshizanzhubenzhan？.htm\" target=\"_blank\"><font color='#008000'>采用什么方式赞助本站？</font></a></li>");
+document.write("<li><a href=\"/gonggao/2009/1002/caiyongshimefangshizanzhubenzhan?.htm\" target=\"_blank\"><font color='#008000'>采用什么方式赞助本站?</font></a></li>");
 
-document.write("<li><a href=\"/gonggao/2009/1002/benzhanchengxuyiquanbukaiyuan,xuexihuodangercikaifadipengyoukeyimianfeihuodeidaima.htm\" target=\"_blank\"><font color='#FF0000'>本站程序已全部开源,学习和当二次开发的朋友可以免费获得代码</font></a></li>");
+document.write("<li><a href=\"/gonggao/2009/1002/benzhanchengxuyiquanbukaiyuan(xuexihuodangercikaifadipengyoukeyimianfeihuodeidaima).htm\" target=\"_blank\"><font color='#FF0000'>本站程序已全部开源(学习和当二次开发的朋友可以免费获得代码)</font></a></li>");
