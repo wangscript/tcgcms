d4961e5bab599712009b2b73ecffb4754734ff8f
sanyungui <sanyungui@yahoo.com.cn>
2009-10-08 07:30:55 +0000

    准备发布的第一版

--- src/WebUI/js/news.js
+++ src/WebUI/js/news.js
@@ -1,16 +1,16 @@
 
-document.write("<li><a href=\"/news/2009/1003/DZ.NETluntanshangchangpiliangjiarudiqudijueben.htm\" target=\"_blank\">DZ.NET 论坛商场批量加入地区的脚本</a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/sheji/2009/1008/photoshopgeyintucengdihanyi.htm\" target=\"_blank\">photoshop盖印图层的含义</a><span>09.27.08</span></li>");
 
-document.write("<li><a href=\"/news/2009/1003/SQLSERVERzuolianjiefuwuqidiyufa.htm\" target=\"_blank\">SQL SERVER做链接服务器的愈发</a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/sheji/2009/1008/AS3jiazaiCSS.htm\" target=\"_blank\">AS3加载CSS</a><span>09.27.08</span></li>");
 
-document.write("<li><a href=\"/gonggao/2009/1002/caiyongshimefangshizanzhubenzhan？.htm\" target=\"_blank\"><font color='#008000'>采用什么方式赞助本站？</font></a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/sheji/2009/1008/flashas3jianshandihtmlwenbenshili.htm\" target=\"_blank\">flashas3简单的html文本实例</a><span>09.27.08</span></li>");
 
-document.write("<li><a href=\"/gonggao/2009/1002/benzhanchengxuyiquanbukaiyuan,xuexihuodangercikaifadipengyoukeyimianfeihuodeidaima.htm\" target=\"_blank\"><font color='#FF0000'>本站程序已全部开源,学习和当二次开发的朋友可以免费获得代码</font></a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/sheji/2009/1007/jueduoyuhuduodizhuanhuan.htm\" target=\"_blank\">角度与弧度的转换</a><span>09.27.08</span></li>");
 
-document.write("<li><a href=\"/news/2009/0930/jianrongIEhuoFFdiCSSwenzihuanxingxiangxixieyue.htm\" target=\"_blank\">兼容IE和FF的CSS文字换行详细解说</a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/help/sandy/2009/1006/shexiangjitiaojiaohuotiaozhengpingmuchicun.htm\" target=\"_blank\">摄像机调焦和调整屏幕尺寸</a><span>09.27.08</span></li>");
 
-document.write("<li><a href=\"/flashsandy/2009/0930/peizhiFlashDevelopdiAS3kaifahuanjing.htm\" target=\"_blank\">配置FlashDevelop的AS3开发环境</a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/sheji/2009/1005/Dictionaryduixiangjianxunhuanfangwen.htm\" target=\"_blank\">Dictionary对象键循环访问</a><span>09.27.08</span></li>");
 
-document.write("<li><a href=\"/version/2009/0930/TCGCMSSystemxitongzuixinbanbenxiazai.htm\" target=\"_blank\">TCGCMSSystem系统最新版本下载</a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/sheji/2009/1005/FLASHAS3duXML.htm\" target=\"_blank\">FLASHAS3读XML</a><span>09.27.08</span></li>");
 
-document.write("<li><a href=\"/anli/2009/0930/xiangtandaixuezhangshaxiaoyoukuai.htm\" target=\"_blank\">湘潭大学长沙校友会</a><span>09.08.03</span></li>");
+document.write("<li><a href=\"/sheji/2009/1005/xiejueFlashDeveloptiaoshibushuchutracexinxi.htm\" target=\"_blank\">解决FlashDevelop调试不输出trace信息</a><span>09.27.08</span></li>");
