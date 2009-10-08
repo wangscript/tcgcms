07be7accb9afc2f7e771d7b7349f5abfbfe95f4b
sanyungui@yahoo.com.cn <sanyungui@yahoo.com.cn@6ccff402-a32b-11de-9ca8-173d21a9ba08>
2009-10-08 08:22:04 +0000

    加入开源说明，正式开源了哇

--- trunk/src/TCG.Controls/TCG.Controls.PageControls/miniPager.cs
+++ trunk/src/TCG.Controls/TCG.Controls.PageControls/miniPager.cs
@@ -1,3 +1,18 @@
+
+/* 
+  * Copyright (C) 2009-2009 tcgcms.com <http://www.tcgcms.cn/> 
+  *  
+  *    本代码以公共的方式开发下载，任何个人和组织可以下载， 
+  * 修改，进行第二次开发使用，但请保留作者版权信息。 
+  *  
+  *    任何个人或组织在使用本软件过程中造成的直接或间接损失， 
+  * 需要自行承担后果与本软件开发者(三晕鬼)无关。 
+  *  
+  *    本软件解决中小型商家产品网络化销售方案。 
+  *     
+  *    使用中的问题，咨询作者QQ邮箱 sanyungui@vip.qq.com 
+  */
+
 namespace TCG.Controls.PageControls
 {
     using System;
