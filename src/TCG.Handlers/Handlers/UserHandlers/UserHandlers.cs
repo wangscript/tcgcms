2e7128fb4fb3d900700ceafe3344143840e27b1e
sanyungui <sanyungui@yahoo.com.cn>
2009-10-22 10:59:02 +0000

    用户注册HTML编写完成

--- src/TCG.Handlers/Handlers/UserHandlers/UserHandlers.cs
+++ src/TCG.Handlers/Handlers/UserHandlers/UserHandlers.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 
 using TCG.Data;
+using TCG.Entity;
 using TCG.Utils;
 
 namespace TCG.Handlers
 {
+
     /// <summary>
     /// 用户登陆相关操作类
     /// </summary>
     public class UserHandlers : ObjectHandlersBase
     {
-        
+        public int Register(User user)
+        {
+
+            return 1;
+        }
     }
 }
