b219cbea55271bb7378ec5a1ec920cdaa04d300d
sanyungui <sanyungui@yahoo.com.cn>
2009-10-25 13:12:06 +0000

    增加用户增加，修改的操作函数

---
+++ sql/update/90_SP_UserManage.sql
@@ -0,0 +1,103 @@
+CREATE PROC SP_UserManage
+(
+	@State INT ,		--
+	@UserLevel INT ,
+	@UserClubLevel INT,
+	@LastLoginTime DATETIME,
+	@Id VARCHAR(36),
+	@PassWord VARCHAR(32),		--
+	@LastLoginIp VARCHAR(32),		--
+	@Name NVARCHAR(50),		--
+	@Email NVARCHAR(50),
+	@action CHAR(2) ='01',		--操作01，添加。02，修改。03，删除
+	@reValue INT OUTPUT 		--返回参数，小于0为异常
+)
+AS
+IF(@action='01')
+BEGIN
+	
+	If(@PassWord='')
+	BEGIN
+		SET @reValue = -1000000090	--密码不能为空！
+		RETURN;
+	END
+	If(@LastLoginIp='')
+	BEGIN
+		SET @reValue = -1000000091	--最后登陆IP不能为空！
+		RETURN;
+	END
+	If(@Name='')
+	BEGIN
+		SET @reValue = -1000000092	--姓名不能为空！
+		RETURN;
+	END
+	
+	INSERT INTO [USER] (State,UserLevel,UserClubLevel,CreateTime,LastLoginTime,Id,PassWord,LastLoginIp,[NAME])
+	VALUES(@State,@UserLevel,@UserClubLevel,GETDATE(),GETDATE(),@Id,@PassWord,@LastLoginIp,@Name)
+	
+	IF(@Email!='')
+	BEGIN
+		INSERT INTO UserContact
+		(
+			UserId,
+			UserRealName,
+			Province,
+			City,
+			District,
+			Fulladdress,
+			Postcode,
+			Telephone,
+			Mobile,
+			Email
+		)
+		VALUES
+		(
+			@Id,
+			'',
+			0,
+			0,
+			0,
+			'',
+			'',
+			'',
+			0,
+			@Email
+		)
+	END
+END
+
+IF(@action='02')
+BEGIN
+	IF(@Id='')
+	BEGIN
+		SET @reValue = -1000000093 --需要修改的用户ID为空
+		RETURN;
+	END
+
+	If(@PassWord='')
+	BEGIN
+		SET @reValue = -1000000090	--密码不能为空！
+		RETURN;
+	END
+	If(@LastLoginIp='')
+	BEGIN
+		SET @reValue = -1000000091	--最后登陆IP不能为空！
+		RETURN;
+	END
+	If(@Name='')
+	BEGIN
+		SET @reValue = -1000000092	--姓名不能为空！
+		RETURN;
+	END
+	
+	UPDATE [USER] SET 
+	  State=@State,UserLevel=@UserLevel,
+	  UserClubLevel=@UserClubLevel,
+	  LastLoginTime=@LastLoginTime,
+	  PassWord=@PassWord,
+	  LastLoginIp=@LastLoginIp
+	  WHERE Id=@Id
+END
+
+
+SET @reValue=1
--- src/TCG.Handlers/Handlers/UserHandlers/UserHandlers.cs
+++ src/TCG.Handlers/Handlers/UserHandlers/UserHandlers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,10 +16,34 @@ namespace TCG.Handlers
     /// </summary>
     public class UserHandlers : ObjectHandlersBase
     {
-        public int Register(User user)
+        /// <summary>
+        /// 用修改，增加操作
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="usercontact"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public int UserManage(User user,UserContact usercontact,string action)
         {
-
-            return 1;
+            SqlParameter sp0 = new SqlParameter("@State", SqlDbType.Int, 4); sp0.Value = (int)user.State;
+            SqlParameter sp1 = new SqlParameter("@UserLevel", SqlDbType.Int, 4); sp1.Value = (int)user.UserLevel;
+            SqlParameter sp2 = new SqlParameter("@UserClubLevel", SqlDbType.Int,  4); sp2.Value = (int)user.UserClubLevel;
+            SqlParameter sp3 = new SqlParameter("@LastLoginTime", SqlDbType.DateTime, 8); sp3.Value = user.LastLoginTime;
+            SqlParameter sp4 = new SqlParameter("@Id", SqlDbType.VarChar, 36); sp4.Value = user.Id;
+            SqlParameter sp5 = new SqlParameter("@PassWord", SqlDbType.VarChar,32); sp5.Value = user.PassWord;
+            SqlParameter sp6 = new SqlParameter("@LastLoginIp", SqlDbType.VarChar, 32); sp6.Value = user.LastLoginIp;
+            SqlParameter sp7 = new SqlParameter("@Name", SqlDbType.NVarChar, 50); sp7.Value = user.Name;
+            SqlParameter sp8 = new SqlParameter("@Email", SqlDbType.NVarChar, 50); sp8.Value = usercontact.Email;
+            SqlParameter sp9 = new SqlParameter("@action", SqlDbType.Char, 2); sp9.Value = action;
+            SqlParameter sp10 = new SqlParameter("@reValue", SqlDbType.Int); sp10.Direction = ParameterDirection.Output;
+            string[] reValues = base.conn.Execute("SP_UserManage", new SqlParameter[] { sp0, sp1, sp2, sp3 ,sp4,sp5,sp6
+            ,sp7,sp8,sp9,sp10}, new int[] { 10 });
+            if (reValues != null)
+            {
+                int rtn = (int)Convert.ChangeType(reValues[0], typeof(int));
+                return rtn;
+            }
+            return -19000000;
         }
 
         /// <summary>
